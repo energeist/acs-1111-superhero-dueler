65b1f8bc3281f172899c35367ccb5f93c0a5ef7b
Mark Rattle <mark.rattle@gmail.com>
2022-11-13 18:08:54 -0500

    Heroes fight with randomly decided order each round until one wins

--- hero.py
+++ hero.py
@@ -83,35 +83,72 @@ class Hero:
     def fight(self, opponent):
         '''
         Current hero will take turns fighting the opponent hero passed in.
+            opponent - hero object
         '''
-        self_score = 0
-        total_health = self.current_health + opponent.current_health
-        if random.randint(1,100) <= (self.current_health / total_health * 100):
-            print(f"{self.hero_name} wins the battle against {opponent.hero_name}!")
-            self_score += 1
+        print(len(self.abilities))
+        print(len(opponent.abilities))
+        if len(self.abilities) == 0 and len(opponent.abilities) == 0:
+            print("Neither hero has any abilities, the fight is a draw!")
         else:
-            print(f"{opponent.hero_name} wins the battle against {self.hero_name}!")
-        return self_score
+            round = 0
+            while self.is_alive() == True and opponent.is_alive() == True:
+                round +=1
+                # randomize the order of attacks for the turn:
+                if random.randint(0,1) == 0:
+                    attack_order = [self, opponent]
+                else:
+                    attack_order = [opponent, self]
+                # first hero attacks second hero
+                print(f"Round {round} - Attack order: {attack_order[0].hero_name} -> {attack_order[1].hero_name}.")
+                attack_order[1].take_damage(attack_order[1].defend(attack_order[0].attack()))
+                print(f"{attack_order[1].hero_name} has {attack_order[1].current_health} HP remaining.")
+                if attack_order[1].is_alive():
+                    attack_order[0].take_damage(attack_order[0].defend(attack_order[1].attack()))
+                    print(f"{attack_order[0].hero_name} has {attack_order[0].current_health} HP remaining.")
+            if self.is_alive() == False:
+                print(f"{opponent.hero_name} wins the fight after {round} rounds!")
+            if opponent.is_alive() == False:
+                print(f"{self.hero_name} wins the fight after {round} rounds!")
+            
+    # def fight(self, opponent):
+    #     '''
+    #     Current hero will take turns fighting the opponent hero passed in.
+    #     '''
+    #     self_score = 0
+    #     total_health = self.current_health + opponent.current_health
+    #     if random.randint(1,100) <= (self.current_health / total_health * 100):
+    #         print(f"{self.hero_name} wins the battle against {opponent.hero_name}!")
+    #         self_score += 1
+    #     else:
+    #         print(f"{opponent.hero_name} wins the battle against {self.hero_name}!")
+    #     return self_score
 
 if __name__ == "__main__":
     my_hero = Hero("Grace Hopper", 200)
-    other_hero = Hero("Papa", 120)
+    other_hero = Hero("Papa", 175)
 
-    ability1 = Ability("Great Debugging", 50)
-    ability2 = Ability("Second Ability", 30)
-    armor1 = Armor("Mithril chain", 50)
-    armor2 = Armor("A big-ass shield", 20)
+    ability1 = Ability("WHERE'S WILL???", 10)
+    ability2 = Ability("WHERE'S WILL??!!!?!?!", 25)
+    armor1 = Armor("Mithril chain", 15)
+    armor2 = Armor("A big-ass shield", 10)
+    ability3 = Ability("Psionic Child Army", 35)
+    ability4 = Ability("Splash", 5)
     my_hero.add_ability(ability1)
     my_hero.add_ability(ability2)
     my_hero.add_armor(armor1)
     my_hero.add_armor(armor2)
-    damage = my_hero.attack()
-    my_hero.take_damage(my_hero.defend(damage))
-    print(my_hero.is_alive())
-    print(my_hero.current_health)
-    my_hero.take_damage(500)
-    print(my_hero.is_alive())
-    print(my_hero.current_health)
+    other_hero.add_ability(ability3)
+    other_hero.add_ability(ability4)
+    other_hero.add_armor(armor1)
+    other_hero.add_armor(armor2)
+    # damage = my_hero.attack()
+    # my_hero.take_damage(my_hero.defend(damage))
+    # print(my_hero.is_alive())
+    # print(my_hero.current_health)
+    # my_hero.take_damage(500)
+    # print(my_hero.is_alive())
+    # print(my_hero.current_health)
+    my_hero.fight(other_hero)
 
 ####TEST CODE AREA####
 
